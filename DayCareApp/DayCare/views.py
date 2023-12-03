e228baf1cf59e04287fccc6424de7d61c898f130
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-12-03 20:35:57 +0200

    clear view.py comments and unused code

--- DayCareApp/DayCare/views.py
+++ DayCareApp/DayCare/views.py
@@ -1,10 +1,8 @@
-from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.contrib.auth import login, logout
 from django.shortcuts import render, redirect, get_object_or_404
 from DayCareApp.DayCare.models import Profile, Parent
 from DayCareApp.DayCare.forms import RegisterUserForm, LoginForm
-from django.contrib.auth.hashers import make_password, check_password
+from django.contrib.auth.hashers import check_password
 
 
 def index(request):
@@ -77,33 +75,6 @@ def login_view(request):
             }
             return render(request, 'registration/login.html', context)
 
-    #     try:
-    #         profile = Profile.objects.get(username=username)
-    #
-    #         if check_password(password, profile.password):
-    #
-    #             profile.is_authenticated = True
-    #             profile.save()
-    #
-    #             return redirect('login_index', profile_id=profile.id)
-    #
-    #         else:
-    #             form = LoginForm()
-    #             context = {
-    #                 'form': form,
-    #                 'error_message': 'Incorrect password. Try again.',
-    #             }
-    #             return render(request, 'registration/login.html', context)
-    #
-    #     except Profile.DoesNotExist:
-    #         form = LoginForm()
-    #         context = {
-    #             'form': form,
-    #             'error_message': 'No such user name. Please try again.',
-    #         }
-    #         return render(request, 'registration/login.html', context)
-    #
-    #
     else:
         form = LoginForm()
 
@@ -112,21 +83,6 @@ def login_view(request):
     }
 
     return render(request, 'registration/login.html', context)
-
-
-# def log_out(request):
-#     user_id = request.session.get('id')
-#
-#     if user_id:
-#         try:
-#             profile = Profile.objects.get(id=user_id)
-#             profile.is_authenticated = False
-#             profile.save()
-#
-#         except Profile.DoesNotExist:
-#             pass
-#     request.session.clear()
-#     return redirect('index')
 
 def log_out(request):
     logout(request)
